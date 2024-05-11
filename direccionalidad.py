409fa885c9e6096e6053bc301a1106d447552664
Martin <mhaimov1995@gmail.com>
2024-05-11 15:24:52 -0300

    agregamos archivos txt respuesta en frec

--- direccionalidad.py
+++ direccionalidad.py
@@ -2,3 +2,8 @@ import pandas as ps
 import numpy as np
 import matplotlib as plt
 
+sens_df = pd.read_excel('Sensibilidad y Directividad - Curvas Smart.xlsx', sheet_name='Sensibilidad')
+sens_values = sens_df.to_numpy()
+
+direc_df = pd.read_excel('Sensibilidad y Directividad - Curvas Smart.xlsx', sheet_name='Directividad')
+direc_values = direc_df.to_numpy()
