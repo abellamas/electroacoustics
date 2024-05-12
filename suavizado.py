e6d6580aa4182b1b058f241703d76a3586ab9c06
abellamas <abellamasm@gmail.com>
2024-05-12 15:27:57 -0300

    suavizado y export de rta freq  finalizado

--- suavizado.py
+++ suavizado.py
@@ -1,33 +1,49 @@
 import numpy as np
 
-# =============================================================================
-# # F debe ser una lista de las frecuencias entregadas por la FFT de menor a mayor
-# # AMP debe ser una lista de las amplitudes en dB de la FFT
-# # OCT debe ser la subdivisión de octava (ej. OCT=3 -> Suavizado por tercios de octava)
-# # OCT=0 es un caso especial donde no se realiza el suavizado y se devuelve la curva intacta
-# # La función devuelve una lista con la curva suavizada de amplitudes en dB.
-# 
-# =============================================================================
-    
-def suavizado(F,AMP,OCT):
-    ampsmooth=AMP
-    if OCT!=0:
-        for n in range(0,len(F)):
-            fsup=F[n]*pow(2,1/(2*OCT))  #calcula el corte superior del promedio
-            finf=F[n]*pow(2,1/(2*OCT))  #calcula el corte inferior del promedio
 
-            if F[-1]<=fsup:
-                idxsup=len(F)-n
-            else:
-                idxsup=np.argmin(abs(F[n:]-fsup))   #busca el índice de fsup
-                
-            if F[0]<=finf:
-                idxinf=np.argmin(abs(F[0:n+1]-finf))    #busca el índice de finf
-            else:
-                idxinf=0
-                
-            if idxsup!= idxinf:
-                temp = pow(10,AMP[idxinf:idxsup+n-1]*0.1) #Aca hay un error
-                print(idxinf, idxsup, n, AMP[0:0])
-                ampsmooth[n] = 10*np.log10(sum(temp)/(len(temp)))
-    return ampsmooth
+def suavizado(f: np.ndarray, amp_db: np.ndarray, oct: int) -> np.ndarray:
+    
+    """
+
+    Devuelve una array de amplitudes suavizada en una fracción de octava.
+
+    Parameters
+    ----------
+    f : numpy.ndarray
+        Frecuencias de menor a mayor
+    amp_db : numpy.ndarray
+        Amplitudes en dB
+    oct: int
+        Fracción o subdivisión de octava para suavizar
+        (ej. OCT=3 -> Suavizado por tercios de octava)
+        oct=0 es un caso especial donde no se realiza el suavizado
+        y se devuelve la curva intacta
+
+    Returns
+    -------
+    out : numpy.ndarray
+        Amplitudes suavizadas
+
+    """
+
+    if oct == 0:
+        return amp_db
+
+    if len(f) != len(amp_db):
+        raise Exception("f y amp_db deben ser del mismo largo.")
+
+    ampsmooth_db = np.zeros(np.size(amp_db))
+    
+    for i, freq in enumerate(f):
+        finf = freq / pow(2, 1 / (2 * oct))  # calcula el corte inferior del promedio
+        fsup = freq * pow(2, 1 / (2 * oct))  # calcula el corte superior del promedio
+
+        idx = np.logical_and(
+            f >= finf, f <= fsup
+        )  # busca los elementos dentro del rango de frecuencias
+
+        # Suma energetica del rango de frecuencias
+        amp = pow(10, amp_db[idx] / 10)
+        ampsmooth_db[i] = 10 * np.log10(sum(amp) / len(amp))
+
+    return ampsmooth_db
